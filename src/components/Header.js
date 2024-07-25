2c85b015b63230bc96218f0d5b4c533c0481ecb7
korede johnson <timilehingabbana502@gmail.com>
2024-07-25 21:32:59 +0100

    baselinkV1

--- src/components/Header.js
+++ src/components/Header.js
@@ -72,7 +72,7 @@ function Header() {
                 whileHover="hover"
                 whileTap="click"
               >
-                Courses
+                Browse courses
               </motion.button>
               <motion.button
                 className="sign-up-button login-button"
